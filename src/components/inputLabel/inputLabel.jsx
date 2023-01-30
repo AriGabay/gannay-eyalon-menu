e62053cb1ee24d2a9d94d0d4ac4f9610d8a9c2a6
AriGabay <arigabay19988@gmail.com>
2023-01-30 18:12:23 +0200

    add auto scroll

--- src/components/inputLabel/inputLabel.jsx
+++ src/components/inputLabel/inputLabel.jsx
@@ -12,14 +12,27 @@ export const InputLabel = ({
       <label className="label-input" htmlFor={inputId}>
         {labelText}
       </label>
-      <textarea
-        name={inputId}
-        onChange={onChange}
-        className="input-area"
-        type={inputType}
-        id={inputId}
-        defaultValue={valueInput[inputId] ?? valueInput[inputId]}
-      />
+      {inputType === 'time' ||
+      inputType === 'date' ||
+      inputType === 'number' ? (
+        <input
+          name={inputId}
+          onChange={onChange}
+          className="input-area"
+          type={inputType}
+          id={inputId}
+          defaultValue={valueInput[inputId] ?? valueInput[inputId]}
+        />
+      ) : (
+        <textarea
+          name={inputId}
+          onChange={onChange}
+          className="input-area"
+          type={inputType}
+          id={inputId}
+          defaultValue={valueInput[inputId] ?? valueInput[inputId]}
+        />
+      )}
     </React.Fragment>
   );
 };
